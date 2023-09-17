a14110f0dfb02279f6bea6dd2e850c0d7ede09ab
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-18 00:06:53 +0530

    navbar added

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types'; // ES6
+
 const Link = ({ route }) => {
   return (
-    <li className="mr-10">
+    <li className="md:mr-10 hover:bg-slate-400 hover:rounded-lg px-6 py-2">
       <a href={route.path}>{route.name}</a>
     </li>
   );
 };
-
+Link.propTypes = {
+route:PropTypes.object
+}
 export default Link;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
-import Link from '../Link/Link'
+import { useState } from "react";
+import Link from "../Link/Link";
+import { RiMenu2Fill } from "react-icons/ri";
+import {  AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About" },
@@ -9,12 +13,26 @@ const NavBar = () => {
     { id: 5, path: "*", name: "NotFound" },
   ];
   return (
-    <nav>
-      <ul className="md:flex">
+    <nav className="text-black p-6 bg-yellow-400">
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
         {
-          routes.map(route => <Link key={route.id} route={route}></Link>)
+          open === true ?
+          <AiOutlineClose></AiOutlineClose>
+         : 
+          <RiMenu2Fill></RiMenu2Fill>
         }
+      </div>
+
+
+    {/*   <ul className={`md:flex absolute duration-1000 ${open?'':'hidden'} bg-yellow-200 px-6 shadow-lg`}> */}
+
+    <ul className={`md:flex md:items-center md:justify-center absolute md:static duration-1000 ${open?'top-16':'-top-60'} bg-yellow-200 px-2 py-2 shadow-lg md:shadow-none rounded-xl`}>
+
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
       </ul>
+      {/* <h1 className="bg-red-100">test</h1> */}
     </nav>
   );
 };
